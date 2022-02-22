cb8add85953a28aca86bf98e2d2b986262afc1c5
oldman <1120665212@qq.com>
2022-02-22 12:34:42 +0800

    first

---
+++ Data/Stock.py
@@ -0,0 +1,8 @@
+
+from jqdatasdk import *
+
+
+# auth('17606513012','3344Woaini')
+
+# df = get_price('000001.XSHE', end_date='2021-05-30 14:00:00',count=5, frequency='1m', fields=['open','close','high','low','volume','money'])
+# print(df)
